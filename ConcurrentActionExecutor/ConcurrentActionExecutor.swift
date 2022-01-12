aeb64d76b2b11b701f6f7adafb1fad48377fc052
Marko Engelman <engelman.marko@gmail.com>
2022-01-12 22:13:24 +0100

    Add 'ConcurrentActionExecutor'.

---
+++ ConcurrentActionExecutor/ConcurrentActionExecutor.swift
@@ -0,0 +1,12 @@
+//
+//  ConcurrentActionExecutor.swift
+//  ConcurrentActionExecutor
+//
+//  Created by Marko Engelman on 12/01/2022.
+//
+
+import Foundation
+
+final class ConcurrentActionExecutor {
+  
+}
