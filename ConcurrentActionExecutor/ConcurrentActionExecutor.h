dce70f939f9b0a34ff9a9ab3acb68d2b00856641
Marko Engelman <engelman.marko@gmail.com>
2022-01-12 22:09:54 +0100

    Initial Commit

---
+++ ConcurrentActionExecutor/ConcurrentActionExecutor.h
@@ -0,0 +1,18 @@
+//
+//  ConcurrentActionExecutor.h
+//  ConcurrentActionExecutor
+//
+//  Created by Marko Engelman on 12/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConcurrentActionExecutor.
+FOUNDATION_EXPORT double ConcurrentActionExecutorVersionNumber;
+
+//! Project version string for ConcurrentActionExecutor.
+FOUNDATION_EXPORT const unsigned char ConcurrentActionExecutorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConcurrentActionExecutor/PublicHeader.h>
+
+
